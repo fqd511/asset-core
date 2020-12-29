988301df0a001340be67a45a7f1961405001f258
fanqidi <fqd511@163.com>
2020-12-29 23:17:18 +0800

    init : type declare

---
+++ type/entity.d.ts
@@ -0,0 +1,43 @@
+// user
+interface IUser extends ICommonAttr {
+	name:string, 
+	id:string, 
+	avatar:string, 
+}
+// record
+interface IRecord extends ICommonAttr {
+	from_account_id:string, 
+	to_account_id:string, 
+	balance:number, 
+	note:string, 
+	tag:record_tag[], 
+	type:record_type[],
+}
+// account
+interface Account extends ICommonAttr {
+	type:account_type, 
+	id:string, 
+	title:string, 
+	balance:number; 	// ￥100.00 => 10000
+}
+// common attribute
+interface ICommonAttr {
+	created_time:number | string, 
+	update_time:number | string, 
+}
+// financial report record
+interface IReportRecord extends ICommonAttr {
+account_id:string, 
+balance:number, 
+year:number, 
+month:number, 
+}
+
+// record tag
+interface ITag extends ICommonAttr {
+	id:string, 
+	label:string, 
+}
+interface record_tag extends ITag{}
+interface account_type extends ITag{}
+interface record_type extends ITag{}
